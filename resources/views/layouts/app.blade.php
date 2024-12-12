4b07b11df2dfcbafbc7f2e320d0c617a7eeb43d6
EvanderCedric <evandercedric25@gmail.com>
2024-12-12 22:22:56 +0700

    reediiting css to work in vercel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,8 +15,12 @@
 
     <!-- Styles -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    
+
     <!-- Bootstrap CSS -->
+    <link href="http://webprog-openmind-qi25vu5hu-evandercedrics-projects.vercel.app/css/bootstrap.css" rel="stylesheet">
+    <link href="https://webprog-openmind-qi25vu5hu-evandercedrics-projects.vercel.app/css/bootstrap.css" rel="stylesheet">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('style.css') }}" rel="stylesheet">
     
